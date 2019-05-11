b9ac9edcd45cceb29a531523a256c5725fe4bfe8
Ashraf Basry <ashrafmn2008@gmail.com>
2019-05-12 00:45:47 +0200

    Init

---
+++ src/VaultController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Basry\Vault;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VaultController extends Controller
+{
+    //
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/Vault', function () {
+	echo "My First Package.";
+});
